6a568ba4c8adfd782044cf2b49ef296d5ffa91d6
Anderson Rodrigo <andersonrodrigo03.14@gmail.com>
2020-10-21 15:25:38 -0300

    Criar View e Controller do Usuario.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RpgMvc.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public string uriBase ="http://andernine.somee.com/RpgApi/Usuarios/";
+        
+    }
+}
